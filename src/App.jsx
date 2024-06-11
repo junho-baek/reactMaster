2f101695319cd23eb2d250d0a92a185aecd9603a
junhoBaek <86907815+junho-baek@users.noreply.github.com>
2024-06-11 10:25:14 +0000

    #2.3 'As' and Attrs (06:18)
    as 와 attrs 메소드의 사용법을 배웠다.
    Father 컴포넌트를 html header 로 사용하는 방법 as 를 쓰면 된다. 버튼 컴포넌트를 a 태그로 바꾸고 싶으면 <Btn as="a"> 라고 쓰면 된다.
    // Input 컴포넌트에 모두 required 라는 속성을 추가시키고 싶다면 이렇게 코딩하면 된다.
    const Input = styled.input.attrs({ required: true })`
      background-color: tomato;
    `;

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,20 @@ const Father = styled.div`
   display: flex;
 `;
 
-
-// 겹치는 부분을 Prop을 이용해서 변수 활용하기
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-// 추가되는 부분은 styled()`(추가할 내용)` 활용하기
-const Circle = styled(Box)`
-  border-radius: 50px;
+// Input 컴포넌트에 모두 required 라는 속성을 추가시키고 싶다면 이렇게 코딩하면 된다.
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    // Father 컴포넌트를 html header 로 사용하는 방법 as 를 쓰면 된다. 버튼 컴포넌트를 a 태그로 바꾸고 싶으면 <Btn as="a"> 라고 쓰면 된다.
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
