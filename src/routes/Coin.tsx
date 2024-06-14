c6ae6b0ef2d698130da5fafa74ff56b368d60260
junhoBaek <86907815+junho-baek@users.noreply.github.com>
2024-06-14 18:34:54 +0000

    #5.8 Nested Routes part Two (08:42)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { Switch, Route, useLocation, useParams } from "react-router";
+import {
+  Switch,
+  Route,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
@@ -35,7 +42,6 @@ const Overview = styled.div`
   padding: 10px 20px;
   border-radius: 10px;
 `;
-
 const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
@@ -48,22 +54,38 @@ const OverviewItem = styled.div`
     margin-bottom: 5px;
   }
 `;
-
 const Description = styled.p`
   margin: 20px 0px;
 `;
 
-// RouteParams 인터페이스 정의: URL 파라미터의 타입을 정의합니다.
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
+
 interface RouteParams {
   coinId: string;
 }
-
-// RouteState 인터페이스 정의: 상태의 타입을 정의합니다.
 interface RouteState {
   name: string;
 }
-
-// InfoData 인터페이스 정의: 코인 정보 데이터의 타입을 정의합니다.
 interface InfoData {
   id: string;
   name: string;
@@ -84,8 +106,6 @@ interface InfoData {
   first_data_at: string;
   last_data_at: string;
 }
-
-// PriceData 인터페이스 정의: 코인 가격 정보 데이터의 타입을 정의합니다.
 interface PriceData {
   id: string;
   name: string;
@@ -127,8 +147,8 @@ function Coin() {
   const { state } = useLocation<RouteState>();
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
-
-  // useEffect 훅: 컴포넌트가 마운트될 때 API로부터 데이터를 Fetch합니다.
+  const priceMatch = useRouteMatch("/:coinId/price");
+  const chartMatch = useRouteMatch("/:coinId/chart");
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -142,7 +162,6 @@ function Coin() {
       setLoading(false);
     })();
   }, [coinId]);
-
   return (
     <Container>
       <Header>
@@ -179,11 +198,21 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
           <Switch>
-            <Route path={`/${coinId}/price`}>
+            <Route path={`/:coinId/price`}>
               <Price />
             </Route>
-            <Route path={`/${coinId}/chart`}>
+            <Route path={`/:coinId/chart`}>
               <Chart />
             </Route>
           </Switch>
