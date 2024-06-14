e19c076d0e714a9ca341220629b8fe8854cdab05
junhoBaek <86907815+junho-baek@users.noreply.github.com>
2024-06-14 22:10:36 +0000

    점검..

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `https://api.coinpaprika.com/v1`;
+const BASE_URL = `/api`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -15,15 +15,9 @@ export function fetchCoinTickers(coinId: string) {
     response.json()
   );
 }
-// export function fetchCoinTickers(coinId: string) {
-//   return fetch(`https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`).then((response) =>
-//     response.json()
-//   );
-// }
+
 export function fetchCoinHistory(coinId: string) {
-  const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
-  return fetch(
-    `https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`
-  ).then((response) => response.json());
+  return fetch(`https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`).then((response) =>
+    response.json()
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reactMaster/', // GitHub repository 이름으로 변경
   server: {
     host: '0.0.0.0',
-  }
-})
+    proxy: {
+      '/api': {
+        target: 'https://api.coinpaprika.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
